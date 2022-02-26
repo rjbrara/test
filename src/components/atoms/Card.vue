10043e96c72db434b577908a0dfcc7e6b7408d70
Taufik Akbar <taufik.akbar@bts.id>
2022-02-26 16:38:57 +0700

    feat: get total funding and get total users

--- src/components/atoms/Card.vue
+++ src/components/atoms/Card.vue
@@ -1,60 +1,31 @@
 <template>
-  <div class="wrap-card">
-    <v-card
-      elevation="2"
-      class="xs-auto my-1"
-      max-width="350px"
-      min-height="150px"
-      rounded="lg"
-    >
-      <v-card-text>
-        <v-row align="center" justify="space-between" class="mx-0">
-          <v-col cols="6">
-            <v-card
-              class="mt-n16 d-flex align-center justify-center"
-              rounded="lg"
-              min-height="100px"
-              min-width="120px"
-              color="red accent-3"
-            >
-              <v-icon color="white" x-large>{{ svgChart }}</v-icon>
-            </v-card>
-          </v-col>
-          <v-col cols="4">
-            <p class="font-weight-regular body-2">Total User</p>
-            <p>123</p>
-          </v-col>
-        </v-row>
-      </v-card-text>
-    </v-card>
-    <v-card
-      elevation="2"
-      class="xs-auto my-1"
-      max-width="350px"
-      min-height="150px"
-      rounded="lg"
-    >
-      <v-card-text>
-        <v-row align="center" justify="space-between" class="mx-0">
-          <v-col cols="6">
-            <v-card
-              class="mt-n16 d-flex align-center justify-center"
-              rounded="lg"
-              min-height="100px"
-              min-width="120px"
-              color="red accent-3"
-            >
-              <v-icon color="white" x-large>{{ svgChart }}</v-icon>
-            </v-card>
-          </v-col>
-          <v-col cols="4">
-            <p class="font-weight-regular body-2">Total User</p>
-            <p>123</p>
-          </v-col>
-        </v-row>
-      </v-card-text>
-    </v-card>
-  </div>
+  <v-card
+    elevation="2"
+    class="xs-auto my-1"
+    max-width="350px"
+    min-height="150px"
+    rounded="lg"
+  >
+    <v-card-text>
+      <v-row align="center" justify="space-between" class="mx-0">
+        <v-col cols="6">
+          <v-card
+            class="mt-n16 d-flex align-center justify-center"
+            rounded="lg"
+            min-height="100px"
+            min-width="120px"
+            color="red accent-3"
+          >
+            <v-icon color="white" x-large>{{ svgChart }}</v-icon>
+          </v-card>
+        </v-col>
+        <v-col cols="4">
+          <p class="font-weight-regular body-2">{{ name }}</p>
+          <p>{{ total }}</p>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -63,6 +34,16 @@ import { ref } from "@vue/composition-api";
 
 export default {
   name: "CardComponent",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+  },
   setup() {
     const svgChart = ref(mdiChartBar);
     return { svgChart };
@@ -70,12 +51,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.wrap-card {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    gap: 2rem
-}
-</style>
+<style></style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,21 +1,64 @@
 <template>
   <DashboardLayout title="Dashboard">
     <template v-slot:child>
-      <Card/>
+      <div class="wrap-card">
+        <Card name="Total Funding" :total="totalFunding" />
+        <Card name="Total Users" :total="totalUsers" />
+      </div>
     </template>
   </DashboardLayout>
 </template>
 
 <script>
 import DashboardLayout from "@/templates/DashboardLayout.vue";
-import { Card } from "@/components"
+import { Card } from "@/components";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
+import { onMounted, reactive, toRefs } from "vue-demi";
+
 export default {
   name: "DashboardPage",
   components: {
     DashboardLayout,
     Card,
   },
+  setup() {
+    const state = reactive({
+      totalFunding: 0,
+      totalUsers: 0,
+    });
+    const getTotalFunding = async () => {
+      const querySnapshot = await getDocs(collection(db, "funding"));
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        Object.keys(data).forEach((key) => {
+          if (key === "currently_collected") {
+            state.totalFunding += data[key];
+          }
+        });
+      });
+    };
+    const getTotalUsers = async () => {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      state.totalUsers = querySnapshot.docs.length;
+    };
+    onMounted(() => {
+      getTotalFunding();
+      getTotalUsers();
+    });
+    return {
+      ...toRefs(state),
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.wrap-card {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 2rem;
+}
+</style>
